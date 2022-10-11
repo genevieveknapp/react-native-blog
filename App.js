f97dfbeac815df48085a9a105123ece8fea05e90
genevieveknapp <genevieve.knapp@gmail.com>
2022-10-11 18:44:35 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import IndexScreen from './src/screens/IndexScreen';
+import { Provider as BlogProvider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
+
+const navigator = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BlogProvider>
+    <NavigationContainer>
+      <navigator.Navigator initialRouteName='Index' screenOptions={{title: 'Blog'}}>
+        <navigator.Screen name='Index' component={IndexScreen} />
+        <navigator.Screen name='Show' component={ShowScreen} />
+        <navigator.Screen name='Create' component={CreateScreen} />
+      </navigator.Navigator>
+    </NavigationContainer> 
+    </BlogProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
